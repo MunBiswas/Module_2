ae0fa60f26c63961753456ec6973ee6f901bd3f8
MunBiswas <ms.mun.biswas@gmail.com>
2024-05-04 18:29:32 +0600

    Add

---
+++ conditional_3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{   
+    int tk;
+    scanf("%d",&tk);
+    if(tk>=5000)
+
+    {   {printf("coxs bazar jabo\n");}
+
+
+     if(tk>=10000)
+ {printf("saint martin jabo\n");}
+
+     else
+ {printf("ferot chole asbo");}}
+
+
+else
+  {printf("kothao jabo na");}
+
+
+
+
+
+
+    return 0;
+}
